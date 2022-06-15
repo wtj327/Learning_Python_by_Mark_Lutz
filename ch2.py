7f0edafac887875aba9a6bd66c2507935a1d9db0
Tianjiao Wang <wtj327@gmail.com>
2022-06-15 19:39:12 -0400

    added ch2

---
+++ ch2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def test_code():
+    print('Ch2 starts ...\n')
+
+    print('hello world')
+    print(2 ** 100)
+
+
+    print('\nCh2 ends ...\n')
+
